12635cbbe1bd015a32d5908994a8629b66394d85
Dmytro Vorobiov <d@dvor.me>
2017-05-20 11:26:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Wiki for iOS inspired by [Vimwiki](https://vimwiki.github.io/).
+
+## Contact
+
+[d@dvor.me](mailto:d@dvor.me?subject=TextWiki)
+
+## License
+
+TextWiki is available under the MIT license. See the [LICENSE](LICENSE) file for more info.
+
---
+++ TextWiki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextWiki
+//
+//  Created by Dmytro Vorobiov on 20/05/2017.
+//  Copyright © 2017 Dmytro Vorobiov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
